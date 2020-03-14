6305259628e48d587d0badfe01ca24f63dd2ada9
Narazaka <info@narazaka.net>
2020-03-14 16:23:02 +0900

    impl

---
+++ convertSurfaceToStandardPng.js
@@ -0,0 +1,84 @@
+// @ts-check
+const Jimp = require("jimp");
+const fs = require("fs");
+
+/**
+ * 
+ * @param {Buffer} data 
+ * @param {number} index 
+ * @param {{r: number; g: number; b: number, a: number}} color 
+ */
+function isBitmapColorSame(data, index, color) {
+    return data[index] == color.r &&
+    color[index + 1] === color.g &&
+    color[index + 2] === color.b &&
+    color[index + 3] === color.a;
+}
+
+/**
+ * 
+ * @param {Buffer} data 
+ * @param {number} index 
+ * @param {{r: number; g: number; b: number, a: number}} color 
+ */
+function setBitmapColor(data, index, color) {
+    data[index] = color.r;
+    data[index + 1] = color.g;
+    data[index + 2] = color.b;
+    data[index + 3] = color.a;
+}
+
+/**
+ * 
+ * @param {import("jimp")} shell 
+ * @param {import("jimp")} [pna] 
+ */
+async function convertSurfaceDataToStandardPngData(shell, pna) {
+    const shellData = shell.bitmap.data;
+
+    if (pna) {
+        const pnaData = pna.bitmap.data;
+        if (pna.bitmap.width !== shell.bitmap.width || pna.bitmap.height !== shell.bitmap.height) {
+            throw new Error(`shell size [${shell.bitmap.width}x${shell.bitmap.height}] !== pna size [${pna.bitmap.width}x${pna.bitmap.height}]`);
+        }
+        shell.scan(0, 0, shell.bitmap.width, shell.bitmap.height, (_x, _y, index) => {
+            shellData[index + 3] = pnaData[index];
+        });
+    }
+
+    const transparentTargetColor = Jimp.intToRGBA(shell.getPixelColor(0, 0));
+    const transparentColor = {...transparentTargetColor, a: 0};
+    shell.scan(0, 0, shell.bitmap.width, shell.bitmap.height, (_x, _y, index) => {
+        if (isBitmapColorSame(shellData, index, transparentTargetColor)) {
+            setBitmapColor(shellData, index, transparentColor);
+        }
+    });
+}
+
+/**
+ * 
+ * @param {string} shellPath 
+ */
+async function convertSurfaceToStandardPngData(shellPath) {
+    if (!fs.existsSync(shellPath)) throw new Error(`Cannot find file [${shellPath}]`);
+    const shell = await Jimp.read(shellPath);
+    const pnaPath = shellPath.replace(/\.png$/, ".pna");
+    const pna = fs.existsSync(pnaPath) ? await Jimp.read(pnaPath) : undefined;
+    convertSurfaceDataToStandardPngData(shell, pna);
+    return shell;
+}
+
+/**
+ * 
+ * @param {string} shellPath 
+ * @param {string} destinationPath 
+ */
+async function convertSurfaceToStandardPng(shellPath, destinationPath) {
+    await (await convertSurfaceToStandardPngData(shellPath)).writeAsync(destinationPath);
+}
+
+module.exports = {
+    convertSurfaceDataToStandardPngData,
+    convertSurfaceToStandardPngData,
+    convertSurfaceToStandardPng,
+};
